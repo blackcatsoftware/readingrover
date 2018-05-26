dcf9186a4e0ded105ffb05a287fac5b9492590e9
Jeff Dezso <jeff.dezso@retailerwebservices.com>
2018-05-25 20:42:20 -0700

    Initial Java project creation

---
+++ src/main/java/net/jdkr/readingrover/App.java
@@ -0,0 +1,13 @@
+package net.jdkr.readingrover;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
